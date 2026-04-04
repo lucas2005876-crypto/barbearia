4d4a35ebcd7d58cb8ca26defe1649ea55cdf2334
Lucas <lucasmicaoliveira@gmail.com>
2026-04-04 14:54:21 -0300

    Refine comments and logging in Barbearia class
    Updated comments for clarity and added a log for the number of clients served.

--- src/Barbearia.java
+++ src/Barbearia.java
@@ -3,12 +3,11 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
 
 public class Barbearia {
-    // Semáforos para controle de capacidade (O "Guarda" da barbearia)
+    // Semáforos para controle de capacidade (O "Guarda" da barbearia) o true permite a justiça FIFO
     private final Semaphore semaforoTotal = new Semaphore(17, true);
     private final Semaphore semaforoSofa = new Semaphore(4, true);
     private final Semaphore semaforoPagamento = new Semaphore(1, true);
-
-    // Filas que garantem o FIFO explícito e evitam Busy-Wait
+    
     private final BlockingQueue<Cliente> filaEmPe = new ArrayBlockingQueue<>(13);
     private final BlockingQueue<Cliente> filaSofa = new ArrayBlockingQueue<>(4);
 
@@ -18,7 +17,6 @@ public class Barbearia {
 
     // O cliente chama este método ao chegar
     public void entrar(Cliente cliente) throws InterruptedException {
-        // Tenta adquirir vaga no recinto (max 20)
             semaforoTotal.acquire();
             filaEmPe.put(cliente); // put() bloqueia se estiver cheio, mas aqui o semáforo garante vaga
             System.out.println(cliente.getNameCliente() + " entrou na barbearia");
@@ -33,7 +31,6 @@ public class Barbearia {
         System.out.println(cliente.getNameCliente() + " sentou no sofá.");
     }
 
-    // O Barbeiro chama este método para pegar o próximo do sofá
     public Cliente chamarProximoDoSofa() throws InterruptedException {
         Cliente proximo = filaSofa.take(); // Bloqueia o barbeiro se o sofá estiver vazio
         System.out.println(proximo.getNameCliente() + " foi chamado para cortar o cabelo");
@@ -49,6 +46,8 @@ public class Barbearia {
         System.out.println(cliente.getNameCliente() + " pagou e saiu.");
         semaforoPagamento.release(); // Libera o caixa para o próximo par
         semaforoTotal.release();     // Libera a vaga na barbearia (o cliente saiu do prédio)
+
+        // logs e contador de clientes
         clientesAtendidos++;
         System.out.println("Clientes atendidos: " + clientesAtendidos);
         if (clientesAtendidos >= 25) {
